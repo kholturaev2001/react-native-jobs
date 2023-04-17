029dde98411ea4bf8ddc9854c1d05211ee74cc61
mirakov <holturaevm@gmail.com>
2023-04-17 14:27:34 +0500

    work on Popularjobs component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
-import { 
+import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
-  Image, 
+  Image,
   FlatList
- } from 'react-native'
- import { useRouter } from 'expo-router'
+} from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants'
 
+const jobTypes = ['Full-time', 'Part-time', 'Contractor']
+
 const Welcome = () => {
+  const router = useRouter(); 
+  const [activeJobType, setActiveJobType] = useState('Full-time')
+
+
   return (
     <View>
       <View style={styles.container}>
@@ -22,11 +28,42 @@ const Welcome = () => {
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput 
+          <TextInput
             style={styles.searchInput}
+            value=''
+            onChange={() => { }}
+            placeholder='What are you looking for?'
           />
-
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => { }} >
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item)
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)} >{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+
       </View>
     </View>
   )
